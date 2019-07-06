96091ef1fbf40cdadeaa1cb6fc5f379901d5123f
Roman Konstantinovich <krom.xr@gmail.com>
2019-07-07 03:06:36 +0500

    chart

--- public/js/ctrl.roads.js
+++ public/js/ctrl.roads.js
@@ -20,28 +20,51 @@ const Roads = {
     const strNameEl = document.querySelector('.js-steet-name');
     strNameEl.innerHTML = street.pr_name;
     initMap(id);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['январь', 'февраль', 'март', 'апрель', 'май' ],
+          datasets: [{
+              label: '2018 - 2019',
+              data: [400, 360, 500, 100, 800],
+              //backgroundColor: [
+                  //'rgba(255, 99, 132, 0.2)',
+                  //'rgba(54, 162, 235, 0.2)',
+                  //'rgba(255, 206, 86, 0.2)',
+                  //'rgba(75, 192, 192, 0.2)',
+                  //'rgba(153, 102, 255, 0.2)',
+                  //'rgba(255, 159, 64, 0.2)'
+              //],
+              //borderColor: [
+                  //'rgba(255, 99, 132, 1)',
+                  //'rgba(54, 162, 235, 1)',
+                  //'rgba(255, 206, 86, 1)',
+                  //'rgba(75, 192, 192, 1)',
+                  //'rgba(153, 102, 255, 1)',
+                  //'rgba(255, 159, 64, 1)'
+              //],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
+
+
+
+
+
   },
 
-  renderFilers: async function() {
-
-    const html = `
-       <li class="navRoad"><a href="#" class='active'> Все</a></li>
-       <li class="navRoad"><a href="#">Проблемные</a></li>
-       <li class="navRoad"><a href="#">Беспроблемные</a></li>
-       <li class="navRoad"><a href="#">ДТП</a></li>
-       <li class="navRoad"><a href="#">Ямы</a></li>
-       <li class="navRoad"><a href="#">Нет разметки</a></li>
-       <li class="navRoad"><a href="#">Нет знаков</a></li>
-    `;
-
-
-    const menuHolder = document.querySelector('.js-filters-list');
-    if (menuHolder)
-      menuHolder.innerHTML = html;
-
-
-
-
-  },
 
 }
